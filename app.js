4f0c6d57dbe2c3a021ad4946b1cd2619b920dead
liufei <1196778674@qq.com>
2017-11-23 16:54:24 +0800

    index

--- app.js
+++ app.js
@@ -4,9 +4,10 @@ App({
    */
   onLaunch: function () {
     //调用API从本地缓存中获取数据
-    var user = wx.getStorageSync('user') || []
-    user.unshift(Date.now())
-    wx.setStorageSync('user', user)
+    var userInfo = wx.getStorageSync('userInfo') || [];
+    if (!userInfo.length) {
+      this.getUserInfo();
+    }
   },
 
   /**
@@ -32,26 +33,18 @@ App({
 
   getUserInfo: function (cb) {
     var that = this
-    this.globalData.user = "小灰灰";
-    if (this.globalData.userInfo) {
-      typeof cb == "function" && cb(this.globalData.userInfo)
-    } else {
-      //调用登录接口
-      wx.login({
-        success: function () {
-          wx.getUserInfo({
-            success: function (res) {
-              that.globalData.userInfo = res.userInfo
-              typeof cb == "function" && cb(that.globalData.userInfo)
-            }
-          })
-        }
-      })
-    }
-  },
-  globalData: {
-    userInfo: null,
-    user: '',
+    //调用登录接口
+    wx.login({
+      success: function () {
+        wx.getUserInfo({
+          success: function (res) {
+            wx.setStorage({
+              key: 'userInfo',
+              data: res.userInfo
+            });
+          }
+        })
+      }
+    })
   }
-
 })
