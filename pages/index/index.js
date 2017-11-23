68681ed8a39afaa8be098b5e368c4dd0e2e71d21
liufei <1196778674@qq.com>
2017-11-23 17:13:01 +0800

    index

--- pages/index/index.js
+++ pages/index/index.js
@@ -59,11 +59,7 @@ Page({
     console.log("clicked confirm");
   },
   data: {
-    imgUrls: [
-      'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
-      'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
-      'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
-    ],
+    imgUrls: ['../../images/app.png', '../../images/app.png', '../../images/app.png'],
     indicatorDots: false,
     autoplay: false,
     interval: 100,
